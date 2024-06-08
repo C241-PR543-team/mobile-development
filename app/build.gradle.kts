dd9584470ae4701a1065e6da0a2adbf8adf02702
Alfisah Damayanti <alfisahdamayanti135@gmail.com>
2024-06-08 18:24:16 +0700

    update login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+
+    id("kotlin-kapt")
 }
 
 android {
@@ -15,6 +18,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        kapt {
+//            arguments {
+//                arg("room.schemaLocation", "$projectDir/schemas")
+//            }
+//        }
     }
 
     buildTypes {
@@ -33,6 +41,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -42,7 +54,36 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("org.mockito:mockito-inline:3.12.4")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.0")
+    implementation("androidx.room:room-ktx:2.4.2")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.0")
+    implementation("androidx.room:room-paging:2.4.0-rc01")
+    implementation("androidx.room:room-runtime:2.6.1")
+    //kapt("androidx.room:room-compiler:2.4.0")
 }
--- app/src/main/java/com/moove/mooveapp/MainActivity.kt
+++ app/src/main/java/com/moove/mooveapp/MainActivity.kt
@@ -1,20 +1,99 @@
 package com.moove.mooveapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Html
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mSlideViewPager: ViewPager
+    private lateinit var mDotLayout: LinearLayout
+    private lateinit var backBtn: Button
+    private lateinit var nextBtn: Button
+    private lateinit var skipBtn: Button
+
+    private lateinit var dots: Array<TextView>
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        backBtn = findViewById(R.id.btn_back)
+        nextBtn = findViewById(R.id.btn_next)
+        skipBtn = findViewById(R.id.btn_skip)
+
+        backBtn.setOnClickListener {
+            if (getItem(0) > 0) {
+                mSlideViewPager.setCurrentItem(getItem(-1), true)
+            }
         }
+
+        nextBtn.setOnClickListener {
+            if (getItem(0) < 3) {
+                mSlideViewPager.setCurrentItem(getItem(1), true)
+            } else {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        skipBtn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        mSlideViewPager = findViewById(R.id.slideViewPager)
+        mDotLayout = findViewById(R.id.indicator_layout)
+
+        viewPagerAdapter = ViewPagerAdapter(this)
+        mSlideViewPager.adapter = viewPagerAdapter
+
+        setUpIndicator(0)
+        mSlideViewPager.addOnPageChangeListener(viewListener)
+    }
+
+    private fun setUpIndicator(position: Int) {
+        dots = Array(4) { TextView(this) }
+        mDotLayout.removeAllViews()
+
+        for (i in dots.indices) {
+            dots[i] = TextView(this).apply {
+                text = Html.fromHtml("&#8226")
+                textSize = 35f
+                setTextColor(resources.getColor(R.color.barn_red, applicationContext.theme))
+            }
+            mDotLayout.addView(dots[i])
+        }
+
+        dots[position].setTextColor(resources.getColor(R.color.beige, applicationContext.theme))
+    }
+
+    private val viewListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+        }
+
+        override fun onPageSelected(position: Int) {
+            setUpIndicator(position)
+            backBtn.visibility = if (position > 0) View.VISIBLE else View.INVISIBLE
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {}
+    }
+
+    private fun getItem(i: Int): Int {
+        return mSlideViewPager.currentItem + i
     }
 }
