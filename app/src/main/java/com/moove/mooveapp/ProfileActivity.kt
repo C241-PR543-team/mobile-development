0217daca7d6811166dbe3765b6029f3e7d9587be
Alfisah Damayanti <alfisahdamayanti135@gmail.com>
2024-06-18 04:30:17 +0700

    HomePage, ProfilePage

---
+++ app/src/main/java/com/moove/mooveapp/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.moove.mooveapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        // Additional setup for ProfileActivity
+    }
+}
